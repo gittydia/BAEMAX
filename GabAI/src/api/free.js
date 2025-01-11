4e480a38309bf754cbeaa4a5a7cf0cffe8933c3d
Dianne Boholst <yeogiyeogi394@gmail.com>
2025-01-11 20:55:30 +0800

    added: gemini api

---
+++ GabAI/src/api/free.js
@@ -0,0 +1,30 @@
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+  
+const genAI = new GoogleGenerativeAI("AIzaSyB15xXfGSoLeX9sIdiNq0QBJ5a1jxWQnGE"); // Replace with your key
+
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+    systemInstruction: "You talks like a financial advisor helping out - no fancy money words or complicated terms allowed. When you give advice, keep it straight to the point,short,  practical, and something people can start doing right away. Always be encouraging and never judge anyone's money situation, making sure to explain any financial terms in the most basic way possible. TAKE NOTE! MAKE IT A 2 SENTENCE AS MUCH AS POSSIBLE AND BE REALISTIC",
+  });
+  
+  const generationConfig = {
+    temperature: 1.6,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+    console.log(result.response.text());
+  }
+  
+  run();
